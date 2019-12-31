ea7e63b0f10692aaa52460ae2cc8c70d1625427a
liyanming236 <2364147809@qq.com>
2019-12-31 14:43:00 +0800

    删除更新

--- src/com/atguigu/bean/Hello.java
+++ src/com/atguigu/bean/Hello.java
@@ -4,18 +4,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("你好!");
-		System.out.println("你好啊！");
-		System.out.println("好好好!");
-		System.out.println("好么");
-		System.out.println("试试啥");
-		System.out.println("更新6");
-		System.out.println("更新7");
-		System.out.println("更新8");
-		System.out.println("更新8");
-		System.out.println("更新999");
-		System.out.println("更新10");
-		System.out.println("测试分支");
-		System.out.println("测试分支222");
 		
 	}
 
